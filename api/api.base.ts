c863e8bce305689fb90babd7a890d2650e155a5c
Oleksandr Holovin <emanuel@conicapps.com>
2023-07-22 20:05:28 +0300

    axios interceptor updated

--- api/api.base.ts
+++ api/api.base.ts
@@ -1,97 +1,79 @@
-import axios, { Method } from 'axios';
-import { store } from '../../redux';
-// import { logout } from '../redux/actions/auth.actions';
-import AlerterService from '../services/AlertService';
-import { PayloadMessage, PayloadError } from '../types';
+import axios from 'axios';
+import intercept from './interceptor';
+import { ApiResponse } from '../types';
+import { handleResponseError as handleError } from '../utils';
 
-interface IApiResponse {
-    success: boolean;
-    errors: PayloadError[];
-    messages: PayloadMessage[];
-    value?: any;
-}
-
-export const baseUrl = 'https://localhost:3000';
-
-axios.defaults.baseURL = baseUrl;
-
-const returnAuthorizationHeader = (headers: any) => {
-    const token = store.getState().token.accessToken;
-
-    return !token
-        ? { ...headers }
-        : {
-            ...headers,
-            Authorization: `Bearer ${token}`,
-        };
+type KeyValue<U> = {
+	[key: string]: U;
 };
 
-const logoutIfNoAccess = (response: any) => {
-    const { dispatch } = store;
-    if (response.data.status === 403) {
-        AlerterService.error(response.data.title);
-        //   dispatch(logout());
-    }
-    if (response.status === 403) {
-        AlerterService.error(response.title);
-        // dispatch(logout());
-    }
+intercept();
+
+export const API = {
+	get: async (url: string, params: any, headers: KeyValue<string> = {}) => {
+		const response = await axios
+			.get(url, {
+				params,
+				headers: {
+					...headers,
+				},
+			})
+			.catch(err => handleError(err));
+		return response.data;
+	},
+	post: async (
+		url: string,
+		body: Record<string, any>,
+		headers: KeyValue<string> = {},
+	): Promise<ApiResponse<any>> => {
+		const response = await axios
+			.post(url, body, {
+				headers: {
+					...headers,
+				},
+			})
+			.catch(err => handleError(err));
+		return response.data;
+	},
+	put: async (
+		url: string,
+		body: Record<string, any>,
+		headers: KeyValue<string> = {},
+	) => {
+		const response = await axios
+			.put(url, body, {
+				headers: {
+					...headers,
+				},
+			})
+			.catch(err => handleError(err));
+		return response.data;
+	},
+	patch: async (
+		url: string,
+		body: Record<string, any>,
+		headers: KeyValue<string> = {},
+	) => {
+		const response = await axios
+			.patch(url, body, {
+				headers: {
+					...headers,
+				},
+			})
+			.catch(err => handleError(err));
+		return response.data;
+	},
+	delete: async (
+		url: string,
+		body?: Record<string, any>,
+		headers: KeyValue<string> = {},
+	) => {
+		const response = await axios
+			.delete(url, {
+				data: body,
+				headers,
+			})
+			.catch(err => handleError(err));
+		return response.data;
+	},
 };
-
-class Api {
-    async get(url: string, headers: any = {}) {
-        const response = await axios
-            .get(url, { headers: returnAuthorizationHeader(headers) })
-            .catch((err) => ({ data: err.response.data }));
-        logoutIfNoAccess(response);
-
-        return response.data;
-    }
-
-    async post(
-        url: string,
-        body: any = null,
-        headers: any = {},
-    ): Promise<IApiResponse> {
-        const response = await axios
-            .post(url, body, { headers: returnAuthorizationHeader(headers) })
-            .catch((err) => ({ data: err.response.data }));
-        logoutIfNoAccess(response);
-
-        return response.data;
-    }
-
-    async put(url: string, body: any = null, headers: any = {}) {
-        const response = await axios
-            .put(url, body, { headers: returnAuthorizationHeader(headers) })
-            .catch((err) => ({
-                data: err.response.data,
-            }));
-        logoutIfNoAccess(response);
-        return response.data;
-    }
-
-    async patch(url: string, body: any = null, headers: any = {}) {
-        const response = await axios
-            .patch(url, body, { headers: returnAuthorizationHeader(headers) })
-            .catch((err) => ({
-                data: err.response.data,
-            }));
-        logoutIfNoAccess(response);
-
-        return response.data;
-    }
-
-    async delete(url: string, body: any = null, headers: any = {}) {
-        const response = await axios
-            .delete(url, { headers: returnAuthorizationHeader(headers) })
-            .catch((err) => ({
-                data: err.response.data,
-            }));
-        logoutIfNoAccess(response);
-
-        return response.data;
-    }
-}
-
-export default new Api();
